1eaf3eb0e35891d70c64727cf1d515c79e529c34
JoelKerslow <joel.kerslow@outlook.com>
2022-10-06 17:33:08 +0200

    Lagt till properties och metod-mallar i Recipe-klassen

--- RecceptHanteringGrupp/Classes/Recipe.cs
+++ RecceptHanteringGrupp/Classes/Recipe.cs
@@ -1,6 +1,44 @@
 ﻿namespace RecceptHanteringGrupp.Classes
 {
-    internal class Recipe
+    public class Recipe
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; } //Kött, Fisk, Sallader, Soppor, Desserter/kakor
+        public Image Picture { get; set; } //Path till bilden. Läggs förslagsvis i Properties.Recources (När man skapar upp ett recept anger man alltså image som t.ex: Properties.Resources.flaskfilégryta_med_champinjoner
+
+        public Recipe(string name, string description, string type, Image picture)
+        {
+            Name = name;
+            Description = description;
+            Type = type;
+            Picture = picture;
+        }
+
+        public List<Recipe> Search(string condition, string category)
+        {
+            //List<Recipe> recipes = FileHandler.GetAllRecipes();
+
+            //var result = recipes.Where(recipe => recipe.Name.Contains(condition) && recipe.Type == category).ToList();
+
+            //return result;
+        }
+
+        public void SaveChanges()
+        {
+
+        }
+
+        public void SaveNew(string name, string description, string type, Image picture)
+        {
+            Recipe recipe = new Recipe(name, description, type, picture);
+            //FileHandler.WriteToFile();
+        }
+
+        private void Delete()
+        {
+
+        }
+
     }
 }
--- RecceptHanteringGrupp/LoginForm.Designer.cs
+++ RecceptHanteringGrupp/LoginForm.Designer.cs
@@ -32,9 +32,10 @@
             // 
             // LoginForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(914, 600);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "LoginForm";
             this.Text = "LoginForm";
             this.Load += new System.EventHandler(this.LoginForm_Load);
