e5186e96770a5330a229e2e7975f52fa52edc13b
Kogrisen <100379902+Kogrisen@users.noreply.github.com>
2022-10-06 15:03:05 +0200

    Tog bort snuskiga usings som inte används

--- RecceptHanteringGrupp/Classes/Recipe.cs
+++ RecceptHanteringGrupp/Classes/Recipe.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RecceptHanteringGrupp.Classes
+﻿namespace RecceptHanteringGrupp.Classes
 {
     internal class Recipe
     {
