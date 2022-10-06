bf2c63c98d868338adc898f75320bc4899801f11
Joel <joel.kerslow@outlook.com>
2022-10-06 15:00:35 +0200

    Lagt till klasser, forms och ändrat namn på MainForm

--- RecceptHanteringGrupp/SearchForm.cs
+++ RecceptHanteringGrupp/SearchForm.cs
@@ -1,8 +1,8 @@
 namespace RecceptHanteringGrupp
 {
-    public partial class Form1 : Form
+    public partial class SearchForm : Form
     {
-        public Form1()
+        public SearchForm()
         {
             InitializeComponent();
             //en första kommentar
